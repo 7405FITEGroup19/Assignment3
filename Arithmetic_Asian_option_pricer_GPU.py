e95d4cca6d2f4522b4c1520a9f8102931a83e31a
Zhao Mingyang <67095541+Zhao-Mingyang@users.noreply.github.com>
2023-04-12 14:08:35 +0800

    Update Arithmetic_Asian_option_pricer_GPU.py

--- Arithmetic_Asian_option_pricer_GPU.py
+++ Arithmetic_Asian_option_pricer_GPU.py
@@ -3,11 +3,12 @@ from scipy.stats import norm
 import math
 from Geometric_Asian_option_pricer import geometric_asia_option
 
+
 # arithmatic Asian option
 def arithmetic_asia_option(S0, sigma, r, T, K, n, option_type, M, cv):
     # the input cv should be 'control variate' meaning using control variate, or others meaning not using the general Monte Carlo
     value_geo = geometric_asia_option(S0, sigma, r, T, K, n, option_type)
-    
+
     Dt = T / n
     cp.random.seed(1)
     Z = cp.random.normal(loc=0, scale=1, size=(M, n))
@@ -19,7 +20,7 @@ def arithmetic_asia_option(S0, sigma, r, T, K, n, option_type, M, cv):
 
     arithmean = cp.mean(Spath, axis=1)
     geomean = cp.exp((1 / n) * cp.sum(cp.log(Spath), axis=1))
-    
+
     arithpayoff, geopayoff = calculate_payoffs(arithmean, geomean, K, r, T, option_type)
 
     if cv == 'control variate':  # monte carlo with control variate
@@ -29,6 +30,7 @@ def arithmetic_asia_option(S0, sigma, r, T, K, n, option_type, M, cv):
 
     return conf
 
+
 def calculate_payoffs(arithmean, geomean, K, r, T, option_type):
     if option_type.lower() == 'call':
         arithpayoff = cp.maximum(arithmean - K, 0) * cp.exp(-r * T)
@@ -37,6 +39,7 @@ def calculate_payoffs(arithmean, geomean, K, r, T, option_type):
         arithpayoff = cp.maximum(K - arithmean, 0) * cp.exp(-r * T)
         geopayoff = cp.maximum(K - geomean, 0) * cp.exp(-r * T)
     return arithpayoff, geopayoff
+
 
 def control_variate(arithpayoff, geopayoff, value_geo, M):
     cov = cp.mean(arithpayoff * geopayoff) - cp.mean(arithpayoff) * cp.mean(geopayoff)
